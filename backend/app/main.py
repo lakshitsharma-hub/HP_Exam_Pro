79cf74aa59314bca245e3c314e26314d1e346fdb
lakshitsharma-hub <lakshitsharma976@gmail.com>
2026-08-07 17:48:51 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -27,7 +27,7 @@ supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 # --- CORS MIDDLEWARE SETUP (Mila kar ek kar diya hai) ---
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://hp-exam-pro.vercel.app", "http://localhost:3000", "http://127.0.0.1:5500", "*"],
+    allow_origins=["https://hp-exam-pro.vercel.app", "http://localhost:3000", "http://127.0.0.1:5500"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
